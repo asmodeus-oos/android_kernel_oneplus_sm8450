632d29d20c73936502f6e8e1ddc72a2adc820f76
asmodeus-oos <mohamed20020093@gmail.com>
2026-08-12 22:00:22 +0300

    sm8450: fix Clang warnings and initialization
    # Conflicts:
    #	drivers/thermal/qcom/cpu_voltage_cooling.c

--- drivers/thermal/qcom/cpu_voltage_cooling.c
+++ drivers/thermal/qcom/cpu_voltage_cooling.c
@@ -178,8 +178,12 @@ static struct cc_limits_data *opp_init(int *cpus)
 {
 	int cpu1, cpu2;
 	struct device *cpu1_dev, *cpu2_dev;
+<<<<<<< HEAD
 	struct limits_freq_table *cpu1_freq_table = NULL;
 	struct limits_freq_table *cpu2_freq_table = NULL;
+=======
+	struct limits_freq_table *cpu1_freq_table = NULL, *cpu2_freq_table = NULL;
+>>>>>>> 2863ca29ab9c (sm8450: fix Clang warnings and initialization)
 	struct limits_freq_table *cpu_freq_table[CPU_MAP_CT];
 	int table_ct[CPU_MAP_CT], ret = 0;
 	struct cc_limits_data *cc_cdev = NULL;
